7e92ebdb59fdeafdd3891fc42a5ab073b48f0b53
wuguoxing <wuguoxing@gdhmail.com>
2024-01-05 11:54:59 +0800

    1/5V2

--- src/demo/index11.vue
+++ src/demo/index11.vue
@@ -6,24 +6,31 @@ let three = ref(null)
 let progress = ref(null)
 let text = ref('');
 let bar = ref(0);
+let threeRun = null
 onMounted(() => {
-const a = new run(three.value,{
-  progressDom:progress.value,
-  Progress:(percent=0) => {
-    text.value = `${percent}%`
-    bar.value = percent;
-  },
-  Complete:() => {}
+  threeRun = new run(three.value, {
+    progressDom: progress.value,
+    Progress: (percent = 0) => {
+      text.value = `${percent}%`
+      bar.value = percent;
+    },
+    Complete: () => {
+    }
+  })
 })
-  console.log(a);
-})
+
+function xh() {
+  threeRun?.destroy()
+}
 </script>
 
 <template>
   <canvas id="three" ref="three"></canvas>
-<!--  <div class="container"></div>-->
-  <div  class="progress" ref="progress">
-    <div class="text">{{text}}</div>
+  <div class="container">
+    <button @click="xh">销毁</button>
+  </div>
+  <div class="progress" ref="progress">
+    <div class="text">{{ text }}</div>
     <div class="progress-content">
       <div class="item" :style="{width:text}"></div>
     </div>
@@ -37,14 +44,16 @@ const a = new run(three.value,{
   height: 100vh;
   z-index: 1;
 }
-.container{
+
+.container {
   position: fixed;
   width: 100%;
   height: 100%;
   z-index: 2;
   background: transparent;
 }
-.progress{
+
+.progress {
   position: fixed;
   display: flex;
   width: 100%;
@@ -53,17 +62,20 @@ const a = new run(three.value,{
   align-items: center;
   flex-direction: column;
   z-index: 3;
-  .text{
+
+  .text {
     font-size: 30px;
     font-weight: bold;
   }
-  .progress-content{
+
+  .progress-content {
     width: 200px;
     height: 10px;
     border: 1px solid #025169;
     border-radius: 50px;
     overflow: hidden;
-    .item{
+
+    .item {
       width: inherit;
       height: inherit;
       background: #92B2B5;
